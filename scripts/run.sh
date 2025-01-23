e6ff3315f5d323b66fe2bc1587523747092427cd
shaozhen.liu <1326330421@qq.com>
2025-01-23 04:55:08 -0500

    fix gcc version error

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=2,3
+# 这里是gcc-5.4.0需要配置的环境变量
+# export PATH=/path/to/install/gcc-5.4/bin:$PATH
+# export LD_LIBRARY_PATH=/path/to/install/gcc-5.4/lib/:/path/to/install/gcc-5.4/lib64:$LD_LIBRARY_PATH
+export PATH=/home/shaozhen.liu/gcc/gcc/bin:$PATH
+export LD_LIBRARY_PATH=/home/shaozhen.liu/gcc/gcc/lib/:/home/shaozhen.liu/gcc/gcc/lib64:$LD_LIBRARY_PATH
+
+export CUDA_VISIBLE_DEVICES=0
 
 python scripts/test_time_compute.py recipes/Llama-3.1-8B-Instruct/diff_of_n.yaml
