603110599d167983e6478bad8b7b5645796e5003
shaozhen.liu <1326330421@qq.com>
2025-03-05 08:49:33 -0500

    update gen.sh

--- gen.sh
+++ gen.sh
@@ -8,7 +8,7 @@ python direct_gen.py \
     --prompt_type deepseek-math \
     --seed 0 \
     --start 0 \
-    --end 100 \
+    --end 20000 \
     --temperature 0.6 \
     --n_sampling 1 \
     --max_tokens_per_call 20480
