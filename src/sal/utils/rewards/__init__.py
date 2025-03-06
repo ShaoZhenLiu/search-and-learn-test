1ab52db78048e2b96d1f3abefb750706486227fb
shaozhen.liu <1326330421@qq.com>
2025-03-06 05:42:13 -0500

    move .rewards to .utils

--- src/sal/utils/rewards/__init__.py
+++ src/sal/utils/rewards/__init__.py
@@ -1,5 +1,6 @@
 """Import reward-related classes and types from the reward module."""
 
 from .reward_types import RewardConfig, RewardFn, RewardInput, RewardOutput, RewardType
+from .math_reward import sal_reward_fn
 
-__all__ = ['RewardFn', 'RewardInput', 'RewardOutput', 'RewardType']
+__all__ = ['RewardFn', 'RewardInput', 'RewardOutput', 'RewardType', 'sal_reward_fn']
--- src/sal/utils/rewards/math_reward.py
+++ src/sal/utils/rewards/math_reward.py
@@ -5,9 +5,13 @@ validate answers when necessary.
 """
 from typing import List, Union
 
-from sal.rewards.global_conf import THOUGHT_DELIMITER_START, THOUGHT_DELIMITER_END  # , OAI_RM_MODEL
-from sal.rewards import RewardConfig, RewardFn, RewardInput, RewardOutput, RewardType
-from sal.rewards.math_utils.utils import extract_answer, grade_answer_sympy, grade_answer_mathd
+from datasets import Dataset
+
+from sal.config import Config
+from sal.utils.rewards.global_conf import THOUGHT_DELIMITER_START, THOUGHT_DELIMITER_END  # , OAI_RM_MODEL
+from sal.utils.rewards.reward_types import RewardConfig, RewardFn, RewardInput, RewardOutput, RewardType
+from sal.utils.rewards.math_utils import extract_answer, grade_answer_sympy, grade_answer_mathd
+
 
 # 这里尝试使用orm进行评价
 # from deepscaler.system_prompts import ORM_PROMPT
@@ -30,10 +34,10 @@ class RewardMathFn(RewardFn):
     def __call__(self, input: RewardInput) -> RewardOutput:
         assert input.problem_type == RewardType.MATH, \
             "Invalid problem type: expected 'MATH', but got '{}'".format(input.problem_type)
-        
+
         problem = input.problem
         model_response = input.model_response
-        
+
         # Extract solution.
         if self.config.check_think_format:
             if THOUGHT_DELIMITER_START in model_response and THOUGHT_DELIMITER_END in model_response:
@@ -42,7 +46,7 @@ class RewardMathFn(RewardFn):
                 return RewardOutput(reward=self.config.format_error_reward, is_correct=False)
         else:
             model_solution = model_response
-        
+
         model_answer = extract_answer(model_solution)
         if model_answer is None:
             return RewardOutput(reward=self.config.format_error_reward, is_correct=False)
@@ -51,11 +55,11 @@ class RewardMathFn(RewardFn):
         ground_truths = input.ground_truth.get("answer", None)
         if ground_truths is None:
             return RewardOutput(reward=self.config.unk_error_reward, is_correct=False)
-        
+
         # Convert single answer to list for uniform processing
         if isinstance(ground_truths, (str, float, int)):
             ground_truths = [ground_truths]
-            
+
         # Process each ground truth
         processed_ground_truths = []
         for truth in ground_truths:
@@ -66,13 +70,14 @@ class RewardMathFn(RewardFn):
                     processed_ground_truths.append(processed_truth)
             else:
                 processed_ground_truths.append(truth)
-        
+
         if not processed_ground_truths:
             return RewardOutput(reward=self.config.unk_error_reward, is_correct=False)
 
         # Check against all possible correct answers
         for ground_truth in processed_ground_truths:
-            is_correct = grade_answer_mathd(model_answer, ground_truth) or grade_answer_sympy(model_answer, ground_truth)
+            is_correct = grade_answer_mathd(model_answer, ground_truth) or grade_answer_sympy(model_answer,
+                                                                                              ground_truth)
             if is_correct:
                 return RewardOutput(reward=self.config.correct_reward, is_correct=True)
 
@@ -96,14 +101,16 @@ class RewardMathFn(RewardFn):
         #                 temperature=0.0,
         #                 model_id=OAI_RM_MODEL,
         #             )
-                    
+
         #             if "[[YES]]" in orm_response:
         #                 return RewardOutput(reward=self.config.correct_reward, is_correct=True)
         #             continue
-                
+
         return RewardOutput(reward=self.config.incorrect_reward, is_correct=False)
 
-def sal_reward_fn(solution_str: str, ground_truth: Union[str, List[str]], enable_llm: bool = False, check_think: bool = False):
+
+def _sal_reward_fn(solution_str: str, ground_truth: Union[str, List[str]], enable_llm: bool = False,
+                   check_think: bool = False):
     """reward_function: implimented both rule-based and orm to deal woth LLM math response checking
 
     Args:
@@ -119,8 +126,33 @@ def sal_reward_fn(solution_str: str, ground_truth: Union[str, List[str]], enable
     reward_config.use_math_orm = enable_llm
     reward_config.check_think_format = check_think
     reward_fn = RewardMathFn(reward_config)
-    reward_response = reward_fn(RewardInput(problem=solution_str, problem_type=RewardType.MATH, model_response=solution_str, ground_truth={"answer": ground_truth}))
+    reward_response = reward_fn(
+        RewardInput(problem=solution_str, problem_type=RewardType.MATH, model_response=solution_str,
+                    ground_truth={"answer": ground_truth}))
     return reward_response.is_correct
+
+
+def sal_reward_fn(dataset: Dataset, config: Config):
+    assert config.approach in ["iter_gen_multi_turn", "diff_of_n_multi_turn"], \
+        NotImplementedError("当前的方法还不能判断生成答案的正确与否，需要进一步验证")
+    dataset = dataset.map(
+        lambda x: {
+            "correct": _sal_reward_fn(
+                solution_str=x["messages"][-1]["content"],  # 最后一个回答会输出在\boxed{}中的答案
+                ground_truth=x["answer"],
+                enable_llm=False, check_think=False,
+            )
+        }
+    )
+
+    if config.filter_correct:
+        total = len(dataset)
+        dataset = dataset.filter(lambda x: x["correct"])  # 只保留正确的数据
+        correct_count = len(dataset)
+        acc = correct_count / total * 100
+        return dataset, acc
+
+    return dataset, None
 
 # if __name__ == "__main__":
 #     reward = RewardMathFn(RewardConfig)
